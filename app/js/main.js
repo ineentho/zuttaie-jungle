233254d01cc0e7ca2ac2573a33647e0bbed08797
Henrik Karlsson <hk.henrik@gmail.com>
2015-06-25 20:38:19 +0200

    David progress

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,17 @@
-import test from './module'
+const lefta=document.querySelector(".lefta");
+lefta.addEventListener("click",function(){
+    alert ("POPUP");
+});
+const righta=document.querySelector(".righta");
+righta.addEventListener("click",function(){
+    alert ("POPUP");
+});
 
-console.log('asd');
-
-test();
+document.body.addEventListener("keydown",function(e){
+    if (e.keyCode==37){
+        alert ("left!");
+    }
+    if (e.keyCode==39){
+        alert ("right!");
+    }
+})
