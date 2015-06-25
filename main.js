6250f4af60b68ad9516c5a6cb98d60bb187466f2
Circle CI <hk.henrik+circle@gmail.com>
2015-06-25 19:30:15 +0000

    Build "David kan också"

--- main.js
+++ main.js
@@ -1,21 +1,36 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 
+var previous = document.querySelector(".previous");
+var next = document.querySelector(".next");
+var current = document.querySelector(".current");
+var slides = ["screenshots/2015-06-23-01.jpg", "screenshots/bluearrow.jpg", "screenshots/pensharpener.jpg", "screenshots/pixelemerald.jpg"];
 var lefta = document.querySelector(".lefta");
+var currentimg = 2;
 lefta.addEventListener("click", function () {
-    alert("POPUP");
+    currentimg--;
+    images();
 });
 var righta = document.querySelector(".righta");
-righta.addEventListener("click", function () {
-    alert("POPUP");
-});
 
+righta.addEventListener("click", function () {
+    currentimg++;
+    images();
+});
 document.body.addEventListener("keydown", function (e) {
     if (e.keyCode == 37) {
-        alert("left!");
+        currentimg--;
+        images();
     }
     if (e.keyCode == 39) {
-        alert("right!");
+        currentimg++;
+        images();
     }
 });
+function images() {
+    previous.src = slides[currentimg - 1];
+    current.src = slides[currentimg];
+    next.src = slides[currentimg + 1];
+}
+images();
 },{}]},{},[1])
