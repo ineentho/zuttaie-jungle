69bc139af18f6a88bb78730ffa9716c2a192a85d
Henrik Karlsson <hk.henrik@gmail.com>
2015-06-25 21:29:33 +0200

    David kan också

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,33 @@
+const previous=document.querySelector(".previous");
+const next=document.querySelector(".next");
+const current=document.querySelector(".current");
+const slides = ['screenshots/2015-06-23-01.jpg','screenshots/bluearrow.jpg','screenshots/pensharpener.jpg','screenshots/pixelemerald.jpg'];
 const lefta=document.querySelector(".lefta");
+let currentimg=2;
 lefta.addEventListener("click",function(){
-    alert ("POPUP");
+    currentimg--;
+    images();
 });
 const righta=document.querySelector(".righta");
-righta.addEventListener("click",function(){
-    alert ("POPUP");
-});
 
+righta.addEventListener("click",function(){
+    currentimg++;
+    images();
+});
 document.body.addEventListener("keydown",function(e){
     if (e.keyCode==37){
-        alert ("left!");
+        currentimg--;
+        images();
     }
     if (e.keyCode==39){
-        alert ("right!");
-    }
+        currentimg++;
+        images();
+     }
 })
+function images()
+{
+    previous.src = slides[currentimg-1]
+    current.src = slides[currentimg]
+    next.src = slides[currentimg+1]
+}
+images();
