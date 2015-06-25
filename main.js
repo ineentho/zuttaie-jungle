2b8704e04dbed3126e85062ed5bf31c736db3ea2
Circle CI <hk.henrik+circle@gmail.com>
2015-06-25 18:40:42 +0000

    Build "Merge branch 'master' of github.com:ineentho/zuttaie-jungle"

--- main.js
+++ main.js
@@ -1,25 +1,21 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-'use strict';
+"use strict";
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-
-var _module2 = require('./module');
-
-var _module3 = _interopRequireDefault(_module2);
-
-console.log('asd');
-
-(0, _module3['default'])();
-},{"./module":2}],2:[function(require,module,exports){
-'use strict';
-
-Object.defineProperty(exports, '__esModule', {
-    value: true
+var lefta = document.querySelector(".lefta");
+lefta.addEventListener("click", function () {
+    alert("POPUP");
+});
+var righta = document.querySelector(".righta");
+righta.addEventListener("click", function () {
+    alert("POPUP");
 });
 
-exports['default'] = function () {
-    console.log('test');
-};
-
-module.exports = exports['default'];
+document.body.addEventListener("keydown", function (e) {
+    if (e.keyCode == 37) {
+        alert("left!");
+    }
+    if (e.keyCode == 39) {
+        alert("right!");
+    }
+});
 },{}]},{},[1])
